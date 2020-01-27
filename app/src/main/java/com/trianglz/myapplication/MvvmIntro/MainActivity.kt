c411d9793229f68970bb65fa6209016041c77729
aly <alyabdelshafy6561@gmail.com>
2020-01-28 00:58:31 +0200

    add life cycle extention

--- app/src/main/java/com/trianglz/myapplication/MvvmIntro/MainActivity.kt
+++ app/src/main/java/com/trianglz/myapplication/MvvmIntro/MainActivity.kt
@@ -4,23 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
 import com.trianglz.myapplication.R
 
 class MainViewModel: ViewModel() {
 
 
     private val _movieList =  MutableLiveData<String>()
-    private val moveList : LiveData<String> = _movieList
-    private var items: LiveData<List<String>>? = null
-    
+    val moveList : LiveData<String> = _movieList
+
+    fun callApiNetwork(){
+        // setting here _movieList
+    }
+
 
 }
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mainViewModel: MainViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        mainViewModel.moveList.observe(this, Observer {
+
+        })
+
     }
 }
